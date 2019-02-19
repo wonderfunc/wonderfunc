a0b1e158855165fc168828a904cc1c8ff50545b2
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2019-02-19 07:13:25 +0000

    change IP and PORT variable names in JSON

--- src/json/JSONReader.java
+++ src/json/JSONReader.java
@@ -6,7 +6,7 @@ public class JSONReader {
 
     public static String read(String key) {
 
-        JSONObject obj = new JSONObject("{\"sourceUnitIP\":\"localhost\", \"sourUnitPort\":\"8081\"}");
+        JSONObject obj = new JSONObject("{\"collectorIP\":\"localhost\", \"collectorPort\":\"8081\"}");
         return obj.getString(key);
 
     }
