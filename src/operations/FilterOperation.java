9f96980c71e652370a528612cd6bb4f6a5b48164
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2019-02-22 06:57:47 +0000

    change operations constructors to receive the stream

--- src/operations/FilterOperation.java
+++ src/operations/FilterOperation.java
@@ -1,14 +1,22 @@
 package operations;
 
+import stream.Stream;
+
 import java.io.Serializable;
 import java.util.function.Predicate;
 
 public class FilterOperation<T extends Serializable> implements Operation {
 
     private Predicate<T> predicate;
+    private Stream stream;
 
-    public FilterOperation(Predicate<T> predicate) {
+    public FilterOperation(Predicate<T> predicate, Stream stream) {
         this.predicate = predicate;
+        this.stream = stream;
     }
 
+    @Override
+    public void put(Serializable data) {
+
+    }
 }
--- src/operations/MapOperation.java
+++ src/operations/MapOperation.java
@@ -1,12 +1,21 @@
 package operations;
 
+import stream.Stream;
+
 import java.io.Serializable;
 import java.util.function.Function;
 
 public class MapOperation<T extends Serializable, R extends Serializable> implements Operation{
     private Function<T, R> function;
+    private Stream stream;
 
-    public MapOperation(Function<T, R> function) {
+    public MapOperation(Function<T, R> function, Stream stream) {
         this.function = function;
+        this.stream = stream;
+    }
+
+    @Override
+    public void put(Serializable data) {
+
     }
 }
