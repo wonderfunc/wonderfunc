a30c0445e8dc5be22be21f86d62274226c3346ae
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2019-03-17 11:55:29 +0000

    Delete useless classes

--- src/interfaces/Target.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-import java.io.Serializable;
-
-public interface Target<T extends Serializable> {
-    void put(T data);
-}
--- src/interfaces/Unit.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-import java.io.Serializable;
-
-public interface Unit<T extends Serializable> {
-    void next(Target<T> target);
-}
