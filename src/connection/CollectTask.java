6aa3f55a153ea7ccf1780966d838cd76f8754660
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2019-03-27 18:20:52 +0000

    check message type by instance type

--- src/connection/CollectTask.java
+++ src/connection/CollectTask.java
@@ -1,5 +1,9 @@
 package connection;
 
+import message.DataMessage;
+import message.EndOfStreamMessage;
+import message.Message;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
@@ -26,7 +30,7 @@ public class CollectTask implements Runnable {
     private void readInput() throws IOException, ClassNotFoundException {
         ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
         Message inputMessage = (Message) inputStream.readObject();
-        if (inputMessage.type() == MessageType.ENDOFSTREAM) streamThread.closeStream();
-        else streamThread.addToOutPutTarget(inputMessage.data());
+        if (inputMessage instanceof EndOfStreamMessage) streamThread.closeStream();
+        else streamThread.addToOutPutTarget(((DataMessage)inputMessage).data());
     }
 }
