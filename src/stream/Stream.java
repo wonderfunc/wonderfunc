797b305753c419db874a5eb5b95d2bd0b6b7545c
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2019-04-01 11:17:58 +0100

    Refactor: extract method

--- src/stream/Stream.java
+++ src/stream/Stream.java
@@ -2,11 +2,9 @@ package stream;
 
 import containers.LambdaContainer;
 import containers.LocalLambdaContainer;
-import nodes.interfaces.CollectNode;
+import nodes.interfaces.*;
 import nodes.SourceNode;
-import nodes.interfaces.FilterNode;
-import nodes.interfaces.MapNode;
-import nodes.interfaces.Node;
+import repositories.algorithmia.AlgorithmiaFunction;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -19,31 +17,49 @@ public class Stream<T extends Serializable> {
     private LambdaContainer currentLambdaContainer;
 
     public Stream(List<T> list) {
-        nodes.add(new SourceNode(list));
-        this.lambdaContainer = new LocalLambdaContainer();
+        this.nodes.add(new SourceNode(list));
+        this.currentLambdaContainer = new LocalLambdaContainer();
     }
 
     public Stream<T> filter(Predicate<T> predicate) {
-        FilterNode<T> node = lambdaContainer.getFilterNodeFrom(predicate);
-        nodes.get(nodes.size() - 1).next(node);
-        nodes.add(node);
+        register(currentLambdaContainer.createNodeFor(predicate));
         return this;
     }
 
-    @SuppressWarnings("unchecked")
     public <R extends Serializable> Stream<R> map(Function<T, R> function) {
-        MapNode<T, R> node = lambdaContainer.getMapNodeFrom(function);
-        nodes.get(nodes.size() - 1).next(node);
-        nodes.add(node);
+        register(currentLambdaContainer.createNodeFor(function));
         return (Stream<R>) this;
     }
 
     public Pipeline collectTo(List<T> list) {
-        CollectNode<T> node = lambdaContainer.getCollectNode(new OutputTarget<>(list));
-        nodes.get(nodes.size() - 1).next(node);
-        //nodes.add(node);
-
+        chain(currentLambdaContainer.createNodeFor(outputOf(list)));
         return new Pipeline<>(nodes);
+    }
+
+    private void register(FilterNode node) {
+        chain(node);
+        append(node);
+    }
+
+    private void register(SynchronousMapNode node) {
+        chain(node);
+        append(node);
+    }
+
+    private void chain(Target target) {
+        lastNode().next(target);
+    }
+
+    private void append(Node node) {
+        nodes.add(node);
+    }
+
+    private Node lastNode() {
+        return nodes.get(nodes.size() - 1);
+    }
+
+    private OutputTarget outputOf(List<T> list) {
+        return new OutputTarget<>(list);
     }
 
     public Stream<T> on(LambdaContainer lambdaContainer) {
